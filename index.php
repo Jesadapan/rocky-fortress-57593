41cd83a5b939f8a62f67998b9acbac8fca1bc280
Jesadapan <71056687+Jesadapan@users.noreply.github.com>
2021-01-04 15:46:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,42 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Harvard University<br>Division of Continuing Education<br></h1>
+         <p>The<a href="#">Harvard Division of Continuing Education</a>  in the<a href="#"> Faculty of Arts & Sciences</a>  comprises several entities, including:
+        <ul>
+            <li><a href="#">Harvard Extension School</li></a> 
+            <li><a href="#">Harvard Summer School</li></a>
+            <li><a href="#">Harvard Institute for Learning in Retirement</li></a>
+            <li><a href="#">Professional Development programs</li></a>
+        </ul>
+    </p>
+    <p>
+        Through these various entities, the division provides a variety of noncredit and credit-bearing programs online and on campus. The <a href="#"> Division of Continuing Education</a> is part of <a href="#">Harvard University</a> and is responsible for extending excellence to students at every stage of learning.
+
+    </p>
+    <h2> Harvard Extension School<BR></h2>
+        <p> 
+            Study part time at Harvard, in <a href="#">evening or online courses</a>. Earn a degree, professional certificate, or take a course for enrichment.</p>
+            
+        <p>
+            At  <a href="#">Harvard Extension School</a>, we offer <a href="#">over 700 courses</a>  to help you gain new perspectives, develop a professional skill, or earn a Harvard credential. Whatever your learning goals, <a href="#">Harvard Extension School</a> can help you achieve them.</p>
+    <h3>Harvard Summer School</h3>
+        <p>
+            The <a href="#">Harvard Summer School</a> makes Harvard’s academic resources -- a distinguished faculty, well-equipped laboratories, <a href="#">fine museums</a>, and a <a href="#">world-class university library</a> -- available to men and women of many ages, backgrounds, and nationalities through an intensive summer session of liberal arts courses in Cambridge and overseas.</p>
+            <p> Our <a href="#">more than 300 summer courses</a> cover a wide range of subjects, including those in science, humanities, writing, economics, computer science, and many foreign languages. Some courses prepare you for advanced studies in science and medicine. Others introduce you to topics like drama, dance, management, and finance.</p>
+                <h4>Learning in retirement</h4>
+                    <P> The <a href="#">Harvard Institute for Learning in Retirement</a> (HILR) is a vibrant community of retired and semi-retired academics and professionals participating in classes and activities at Harvard University.</P>
+                    <P>The Institute’s curriculum reflects the University’s strong tradition of liberal arts and sciences and offers members the chance to explore subjects neglected during the course of demanding careers.</P>
+                        <h5>Professional development</h5>
+                        <p>Through the <a href="#">professional development programs</a> offered by the Harvard Division of Continuing Education, you can keep yourself and your staff up-to-speed in today's ever-evolving technology and business environments.</p>
+                        <p>At Harvard, we help leaders evolve. Here with us, you can discover new ways to foster change, drive growth and innovation, and make an impact globally. You’ll find opportunities in specialized fields like public health and education, as well as options for high-performing managers to advance to senior leadership roles.</p>
+    </header>
+</body>
+</html>
