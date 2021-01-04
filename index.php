2ac4d4894b98f09942a61169a6d9e65d0e11a5a2
Jesadapan <71056687+Jesadapan@users.noreply.github.com>
2021-01-04 15:51:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,42 +1,183 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <header>
-        <h1>Harvard University<br>Division of Continuing Education<br></h1>
-         <p>The<a href="#">Harvard Division of Continuing Education</a>  in the<a href="#"> Faculty of Arts & Sciences</a>  comprises several entities, including:
-        <ul>
-            <li><a href="#">Harvard Extension School</li></a> 
-            <li><a href="#">Harvard Summer School</li></a>
-            <li><a href="#">Harvard Institute for Learning in Retirement</li></a>
-            <li><a href="#">Professional Development programs</li></a>
-        </ul>
-    </p>
-    <p>
-        Through these various entities, the division provides a variety of noncredit and credit-bearing programs online and on campus. The <a href="#"> Division of Continuing Education</a> is part of <a href="#">Harvard University</a> and is responsible for extending excellence to students at every stage of learning.
 
-    </p>
-    <h2> Harvard Extension School<BR></h2>
-        <p> 
-            Study part time at Harvard, in <a href="#">evening or online courses</a>. Earn a degree, professional certificate, or take a course for enrichment.</p>
-            
-        <p>
-            At  <a href="#">Harvard Extension School</a>, we offer <a href="#">over 700 courses</a>  to help you gain new perspectives, develop a professional skill, or earn a Harvard credential. Whatever your learning goals, <a href="#">Harvard Extension School</a> can help you achieve them.</p>
-    <h3>Harvard Summer School</h3>
-        <p>
-            The <a href="#">Harvard Summer School</a> makes Harvard’s academic resources -- a distinguished faculty, well-equipped laboratories, <a href="#">fine museums</a>, and a <a href="#">world-class university library</a> -- available to men and women of many ages, backgrounds, and nationalities through an intensive summer session of liberal arts courses in Cambridge and overseas.</p>
-            <p> Our <a href="#">more than 300 summer courses</a> cover a wide range of subjects, including those in science, humanities, writing, economics, computer science, and many foreign languages. Some courses prepare you for advanced studies in science and medicine. Others introduce you to topics like drama, dance, management, and finance.</p>
-                <h4>Learning in retirement</h4>
-                    <P> The <a href="#">Harvard Institute for Learning in Retirement</a> (HILR) is a vibrant community of retired and semi-retired academics and professionals participating in classes and activities at Harvard University.</P>
-                    <P>The Institute’s curriculum reflects the University’s strong tradition of liberal arts and sciences and offers members the chance to explore subjects neglected during the course of demanding careers.</P>
-                        <h5>Professional development</h5>
-                        <p>Through the <a href="#">professional development programs</a> offered by the Harvard Division of Continuing Education, you can keep yourself and your staff up-to-speed in today's ever-evolving technology and business environments.</p>
-                        <p>At Harvard, we help leaders evolve. Here with us, you can discover new ways to foster change, drive growth and innovation, and make an impact globally. You’ll find opportunities in specialized fields like public health and education, as well as options for high-performing managers to advance to senior leadership roles.</p>
-    </header>
+<head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>Clean Blog - Start Bootstrap Theme</title>
+
+  <!-- Bootstrap core CSS -->
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom fonts for this template -->
+  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+  <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+
+  <!-- Custom styles for this template -->
+  <link href="css/clean-blog.min.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- Navigation -->
+  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+    <div class="container">
+      <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        Menu
+        <i class="fas fa-bars"></i>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="index.html">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="about.html">About</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="post.html">Sample Post</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contact.html">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="site-heading">
+            <h1>Clean Blog</h1>
+            <span class="subheading">A Blog Theme by Start Bootstrap</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <div class="post-preview">
+          <a href="post.html">
+            <h2 class="post-title">
+              Man must explore, and this is exploration at its greatest
+            </h2>
+            <h3 class="post-subtitle">
+              Problems look mighty small from 150 miles up
+            </h3>
+          </a>
+          <p class="post-meta">Posted by
+            <a href="#">Start Bootstrap</a>
+            on September 24, 2019</p>
+        </div>
+        <hr>
+        <div class="post-preview">
+          <a href="post.html">
+            <h2 class="post-title">
+              I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
+            </h2>
+          </a>
+          <p class="post-meta">Posted by
+            <a href="#">Start Bootstrap</a>
+            on September 18, 2019</p>
+        </div>
+        <hr>
+        <div class="post-preview">
+          <a href="post.html">
+            <h2 class="post-title">
+              Science has not yet mastered prophecy
+            </h2>
+            <h3 class="post-subtitle">
+              We predict too much for the next year and yet far too little for the next ten.
+            </h3>
+          </a>
+          <p class="post-meta">Posted by
+            <a href="#">Start Bootstrap</a>
+            on August 24, 2019</p>
+        </div>
+        <hr>
+        <div class="post-preview">
+          <a href="post.html">
+            <h2 class="post-title">
+              Failure is not an option
+            </h2>
+            <h3 class="post-subtitle">
+              Many say exploration is part of our destiny, but it’s actually our duty to future generations.
+            </h3>
+          </a>
+          <p class="post-meta">Posted by
+            <a href="#">Start Bootstrap</a>
+            on July 8, 2019</p>
+        </div>
+        <hr>
+        <!-- Pager -->
+        <div class="clearfix">
+          <a class="btn btn-primary float-right" href="#">Older Posts &rarr;</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <hr>
+
+  <!-- Footer -->
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <ul class="list-inline text-center">
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+            <li class="list-inline-item">
+              <a href="#">
+                <span class="fa-stack fa-lg">
+                  <i class="fas fa-circle fa-stack-2x"></i>
+                  <i class="fab fa-github fa-stack-1x fa-inverse"></i>
+                </span>
+              </a>
+            </li>
+          </ul>
+          <p class="copyright text-muted">Copyright &copy; Your Website 2020</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.min.js"></script>
+
 </body>
+
 </html>
